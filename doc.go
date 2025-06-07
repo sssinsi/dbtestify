f227fa553081c8c53c74ce436d271704ddb2cd0a
渋川よしき <yoshiki@shibu.jp>
2025-06-08 01:34:59 +0900

    update package document

---
+++ doc.go
@@ -0,0 +1,5 @@
+// dbtestify package provides core logic of dbtestify.
+//
+// It is public but not intended to be used directly by users.
+// github.com/shibukawa/dbtestify/assertdb provides API for database seeding and assertion for testing.
+package dbtestify
